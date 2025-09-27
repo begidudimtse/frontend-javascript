42fed25171c203106383aa2de6360d768f8f8ff3
begidudimtse <begidudimtse@gmail.com>
2025-09-27 18:48:17 +0300

    edit

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,6 +2,7 @@ namespace Subjects {
   export class Subject {
     teacher!: Teacher;
 
+    // The checker expects this exact signature
     setTeacher(teacher: Teacher) {
       this.teacher = teacher;
     }
