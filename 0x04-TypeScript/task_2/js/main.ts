035154a2d1d4ff733d680946eabeed90e1bea561
begidudimtse <begidudimtse@gmail.com>
2025-09-27 15:13:42 +0300

    edited

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,46 +1,12 @@
-// Interfaces
-interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
-}
-interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
+// Type predicate to check if employee is a Director
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
 }
 
-// Director class implementing DirectorInterface
-class Director implements DirectorInterface {
-    workFromHome(): string {
-        return "Working from home";
-    }
-    getCoffeeBreak(): string {
-        return "Getting a coffee break";
-    }
-    workDirectorTasks(): string {
-        return "Getting to director tasks";    }
+// Function to execute work depending on employee type
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
 }
-
-// Teacher class implementing TeacherInterface
-class Teacher implements TeacherInterface {
-    workFromHome(): string {
-        return "Cannot work from home";
-    }
-    getCoffeeBreak(): string {
-        return "Cannot have a break";
-    }
-    workTeacherTasks(): string {
-        return "Getting to work";
-    }
-}
-
-// Function to create either a Director or Teacher
-function createEmployee(salary: number | string): Director | Teacher {
-    if (Number(salary) < 500) {
-        return new Teacher();
-    } 
-    return new Director();
-  
-}
-
