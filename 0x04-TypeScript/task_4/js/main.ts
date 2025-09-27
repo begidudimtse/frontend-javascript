d639d8823f595b0663aecf719246a3ebcd4bd83d
begidudimtse <begidudimtse@gmail.com>
2025-09-27 18:51:49 +0300

    Update task_4/js/main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,38 +4,34 @@
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
 
-import CppSubject = Subjects.Cpp;
-import JavaSubject = Subjects.Java;
-import ReactSubject = Subjects.React;
-import Teacher = Subjects.Teacher;
-import Subject = Subjects.Subject;
+namespace Subjects {
+  // Create teacher with C experience
+  export const cTeacher: Teacher = {
+    firstName: "Guillaume",
+    lastName: "Salva",
+    experienceTeachingC: 10,
+  };
 
-// Create teacher with C experience
-export const cTeacher: Teacher = {
-  firstName: "Guillaume",
-  lastName: "Salva",
-  experienceTeachingC: 10,
-};
+  // Create subject instances
+  export const cpp = new Cpp();
+  export const java = new Java();
+  export const react = new React();
 
-// Create subjects
-export const cpp = new CppSubject();
-export const java = new JavaSubject();
-export const react = new ReactSubject();
+  // C++ Subject
+  console.log("C++");
+  cpp.setTeacher(cTeacher);
+  console.log(cpp.getRequirements());
+  console.log(cpp.getAvailableTeacher());
 
-// C++ Subject
-console.log("C++");
-cpp.setTeacher(cTeacher);
-console.log(cpp.getRequirements());
-console.log(cpp.getAvailableTeacher());
+  // Java Subject
+  console.log("Java");
+  java.setTeacher(cTeacher);
+  console.log(java.getRequirements());
+  console.log(java.getAvailableTeacher());
 
-// Java Subject
-console.log("Java");
-java.setTeacher(cTeacher);
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
-
-// React Subject
-console.log("React");
-react.setTeacher(cTeacher);
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+  // React Subject
+  console.log("React");
+  react.setTeacher(cTeacher);
+  console.log(react.getRequirements());
+  console.log(react.getAvailableTeacher());
+}
